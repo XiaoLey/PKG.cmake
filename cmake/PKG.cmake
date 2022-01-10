db1a763556f9bf01e2d9641571e63f2ded8c705d
XiaoLey <xiao.ley@outlook.com>
2022-01-11 01:13:50 +0800

    Added version control for PKG.cmake

--- cmake/PKG.cmake
+++ cmake/PKG.cmake
@@ -1,3 +1,41 @@
+# PKG.cmake - CMake scripts for encapsulating general or multi-component projects
+# ----------------------------------------
+# See https://github.com/XiaoLey/PKG.cmake for usage and update instructions.
+#
+# ----------------------------------------
+# MIT License
+#
+# Copyright (c) 2022 XiaoLey
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ----------------------------------------
+
+
+# cmake_minimum_required(VERSION 3.19 FATAL_ERROR)
+
+# PKG.cmake version control
+set(CURRENT_PKG_VERSION 0.1-release)
+if (NOT "${CURRENT_PKG_VERSION}" MATCHES "-release$")
+    message(WARNING "PKG: The current PKG.cmake is not a stable version, if you want to use stable functions, please download the release version.")
+endif ()
+
+
 #===============================================================================
 #
 # @brief Quickly package projects or multi-component projects
